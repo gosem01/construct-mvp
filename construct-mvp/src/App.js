9ebcb6332ef7e3c2898eed5a586f3ea79f6e742c
GS <gmoney@GSs-Mac-mini.local>
2023-10-18 16:42:01 -0700

    project setup and adding material UI header

---
+++ construct-mvp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header'; // Import the Header component
+
+function App() {
+  return (
+    <div className="App">
+      <Header /> {/* Use the Header component */}
+      {/* Other components or content */}
+    </div>
+  );
+}
+
+export default App;
